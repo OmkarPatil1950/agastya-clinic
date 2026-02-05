8da45d420fb1959413e64ab37aa20c790febe38f
OmkarPatil1950 <121093603+OmkarPatil1950@users.noreply.github.com>
2026-02-05 18:50:27 +0530

    Update PrescriptionController.java

--- src/main/java/com/hospital/prescription/controller/PrescriptionController.java
+++ src/main/java/com/hospital/prescription/controller/PrescriptionController.java
@@ -14,7 +14,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/api/prescriptions")
-@CrossOrigin(origins = {"http://localhost:3000", "http://localhost:3001", "http://localhost:3002", "http://localhost:80", "http://localhost"})
+@CrossOrigin(origins = "*")
 public class PrescriptionController {
 
     @Autowired
