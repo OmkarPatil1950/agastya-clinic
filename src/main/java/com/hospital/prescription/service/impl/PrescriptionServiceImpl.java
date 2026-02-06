2db59254208cc04c66159eb6ce9ef398f16a75c6
OmkarPatil1950 <121093603+OmkarPatil1950@users.noreply.github.com>
2026-02-06 14:46:25 +0530

    Update PrescriptionServiceImpl.java

--- src/main/java/com/hospital/prescription/service/impl/PrescriptionServiceImpl.java
+++ src/main/java/com/hospital/prescription/service/impl/PrescriptionServiceImpl.java
@@ -208,13 +208,14 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 import org.springframework.web.server.ResponseStatusException;
-
+import javax.transaction.Transactional;
 import java.time.LocalDate;
 import java.util.List;
 import java.util.Optional;
 import java.util.stream.Collectors;
 
 @Service
+@Transactional
 public class PrescriptionServiceImpl implements PrescriptionService {
 
     @Autowired
